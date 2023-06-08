dff9bd5c4c633704197a0667cfef2288348c8dc9
YellowQuartz <daniel.ychuang@gmail.com>
2023-06-09 02:20:42 +0800

    Add My Own Player & Policy

---
+++ src/policy/Exorcist.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+
+#include "../state/state.hpp"
+#include "./Exorcist.hpp"
+
+Move Exorcist::get_move(State* state, int depth) {
+    if (!state->legal_actions.size())
+        state->get_legal_actions();
+
+    auto actions = state->legal_actions;
+    return actions[(rand() + depth) % actions.size()];
+}
