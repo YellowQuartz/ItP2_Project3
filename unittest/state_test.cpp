988b35f18812f5f5927fd738e67ddcab865482e4
YellowQuartz <daniel.ychuang@gmail.com>
2023-06-12 11:57:02 +0800

    Testing Amount of Legal Move

--- unittest/state_test.cpp
+++ unittest/state_test.cpp
@@ -4,12 +4,33 @@
 #include "../src/state/state.hpp"
 #include "../src/config.hpp"
 
+#include<stdio.h>
+
+int search(int depth, State* state) {
+    static FILE* fp = fopen("output.txt", "w+");
+    if (depth == 5) {
+        fprintf(fp, "%s\n", state->encode_state().c_str());
+        return 1;
+    } else {
+        int answer = 0;
+        state->get_legal_actions();
+        for (auto move : state->legal_actions) {
+            auto temp = state->next_state(move);
+            answer += search(depth + 1, temp);
+        }
+        return answer;
+    }
+}
+
 
 int main(){
   srand(RANDOM_SEED);
   
-  State state;
-  std::cout << state.encode_output() << std::endl;
+  State* state = new State;
+
+  std::cout << search(0, state) << std::endl;
+
+  /*std::cout << state.encode_output() << std::endl;
   state.get_legal_actions();
   for(auto move: state.legal_actions){
     std::cout << move.first.first << " " << move.first.second << " " << move.second.first << " " << move.second.second << "\n";
@@ -19,6 +40,6 @@ int main(){
     }else{
       std::cout << "NULL\n";
     }
-  }
+  }*/
   return 0;
 }
