98cfadd36789ff044ff8c4603491928336c06352
YellowQuartz <daniel.ychuang@gmail.com>
2023-06-12 13:14:44 +0800

    Add Support For More Output

--- unittest/state_test.cpp
+++ unittest/state_test.cpp
@@ -7,9 +7,28 @@
 #include<stdio.h>
 
 int search(int depth, State* state) {
-    static FILE* fp = fopen("output.txt", "w+");
+    static FILE* fp1 = fopen("output1.txt", "w+");
+    static FILE* fp2 = fopen("output2.txt", "w+");
+    static FILE* fp3 = fopen("output3.txt", "w+");
+    static FILE* fp4 = fopen("output4.txt", "w+");
+    static int count = 0;
+
     if (depth == 5) {
+        FILE* fp;
+        if (count < 15000) {
+            fp = fp1;
+        }
+        else if (count < 30000) {
+            fp = fp2;
+        }
+        else if (count < 45000) {
+            fp = fp3;
+        }
+        else{
+            fp = fp4;
+        }
         fprintf(fp, "%s\n", state->encode_state().c_str());
+        count++;
         return 1;
     } else {
         int answer = 0;
@@ -27,10 +46,13 @@ int main(){
   srand(RANDOM_SEED);
   
   State* state = new State;
+  //state->player = 1;
 
   std::cout << search(0, state) << std::endl;
 
-  /*std::cout << state.encode_output() << std::endl;
+  /*State state;
+
+  std::cout << state.encode_output() << std::endl;
   state.get_legal_actions();
   for(auto move: state.legal_actions){
     std::cout << move.first.first << " " << move.first.second << " " << move.second.first << " " << move.second.second << "\n";
